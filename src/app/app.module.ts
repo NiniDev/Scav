d91b6555d8520dcbd6058b89caa37c713d1bc7fb
NiniDev <linantfin@gmail.com>
2022-06-05 11:02:34 +0200

    Added DatePipe and german translation for month name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import {DatePipe} from '@angular/common';
+import myLocaleDe from '@angular/common/locales/de';
+import {registerLocaleData} from '@angular/common';
+registerLocaleData(myLocaleDe);
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, DatePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
