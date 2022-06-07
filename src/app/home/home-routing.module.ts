e54ee746d931d75aa83b2e8740536674c472d305
NiniDev <linantfin@gmail.com>
2022-06-07 16:13:03 +0200

    Created tabs

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,7 +6,26 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    children: [
+      {
+        path: 'overview',
+        loadChildren: () => import('./overview/overview.module').then( m => m.OverviewPageModule)
+      },
+      {
+        path: 'upcoming',
+        loadChildren: () => import('./upcoming/upcoming.module').then( m => m.UpcomingPageModule)
+      },
+      {
+        path: 'schedule',
+        loadChildren: () => import('./schedule/schedule.module').then( m => m.SchedulePageModule)
+      },
+      {
+        path: 'learn',
+        loadChildren: () => import('./learn/learn.module').then( m => m.LearnPageModule)
+      }
+    ]
+  },
+
 ];
 
 @NgModule({
