0f4504701295af36ae4cef5479fb3c9e8dc4712b
NiniDev <linantfin@gmail.com>
2022-06-19 11:58:30 +0200

    Reformatted code and changed the place for upcoming homework

--- src/app/home/overview/overview.page.ts
+++ src/app/home/overview/overview.page.ts
@@ -155,45 +155,10 @@ export class OverviewPage implements AfterViewInit {
       done: false
     }
   };
-  el;
 
   constructor() { }
 
   ngAfterViewInit() {
-    this.el = document.getElementById('grid');
-  }
-
-  getOrderedHomework() {
-    const homework = [];
-    const hw = this.getUnfinishedHomework();
-    // eslint-disable-next-line guard-for-in
-    for (const id in hw) {
-      const h = hw[id];
-      homework.push(h);
-    }
-    return homework.sort((a, b) => {
-      const aDate = new Date(a.untilDate);
-      const bDate = new Date(b.untilDate);
-      return aDate.getTime() - bDate.getTime();
-    });
-  }
-
-  getUnfinishedHomework() {
-    const homework = [];
-    // eslint-disable-next-line guard-for-in
-    for (const id in this.homework) {
-      const h = this.homework[id];
-      if (!h.done) {
-        homework.push(h);
-      }
-    }
-    return homework;
-  }
-
-  getDiffDays(date: string) {
-    const today = new Date();
-    const until = new Date(date);
-    return Math.abs(Math.floor((until.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)));
   }
 
 }
--- src/app/home/upcoming/upcoming.module.ts
+++ src/app/home/upcoming/upcoming.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { UpcomingPageRoutingModule } from './upcoming-routing.module';
 
 import { UpcomingPage } from './upcoming.page';
+import {OverviewPageModule} from "../overview/overview.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    UpcomingPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        UpcomingPageRoutingModule,
+        OverviewPageModule
+    ],
   declarations: [UpcomingPage]
 })
 export class UpcomingPageModule {}
