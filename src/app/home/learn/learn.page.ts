b7172bac02e2dcd46317fc8611ed0776a5977a86
NiniDev <linantfin@gmail.com>
2022-07-18 20:55:57 +0200

    List for flashcards

--- src/app/home/learn/learn.page.ts
+++ src/app/home/learn/learn.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from "../../services/data.service";
+import {ModalController} from "@ionic/angular";
 
 @Component({
   selector: 'app-learn',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./learn.page.scss'],
 })
 export class LearnPage implements OnInit {
+  flashcardSets = {};
+  flashcardSetKeys = Object.keys(this.flashcardSets);
 
-  constructor() { }
+  constructor(
+    private dataService: DataService,
+  ) {
+    this.dataService.isReady.subscribe((r) => {
+      if (!r) {
+        return;
+      }
+      // get flashcardSets
+      this.dataService.getFlashcardSets().subscribe(fcs => {
+        this.flashcardSets = {};
+        for (const key in fcs) {
+          if (fcs.hasOwnProperty(key)) {
+            this.flashcardSets[fcs[key].id] = fcs[key];
+          }
+        }
+        this.flashcardSetKeys = Object.keys(this.flashcardSets);
+      });
+    });
+  }
 
   ngOnInit() {
+    const flashcardSet = {
+      name: 'Flashcard Set 1',
+      description: 'This is a flashcard set',
+      flashcards: [],
+    };
+    // this.dataService.addFlashcardSet(flashcardSet).then(() => {
+    //   console.log('added flashcard set');
+    // });
   }
 
 }
